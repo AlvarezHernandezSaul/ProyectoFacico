69524041d1aa2aa3bab4fdbbe6c91fccfa2a5989
AlvarezHernandezSaul <al222110813@gmail.com>
2023-07-27 12:10:09 -0600

    main form working

--- RegistrosFacico/app/Http/Controllers/RegistroController.php
+++ RegistrosFacico/app/Http/Controllers/RegistroController.php
@@ -12,26 +12,34 @@ class RegistroController extends Controller
         return view('registro');
     }   
 
-    public function store(Request $request){
-    $request->validate([
-        'nombre' => 'required',
-        'cuenta' => 'required',
-        'servicio' => 'required',
-        'numero_equipo' => 'nullable',
-        'licenciaturas' => 'required',
-        'usuarios' => 'required',
-        'quejas_sugerencias' => 'nullable',
-    ]);
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nombre' => 'required',
+            'cuenta' => 'required',
+            'servicio' => 'required',
+            'numero_equipo' => 'nullable',
+            'licenciaturas' => 'required',
+            'usuario' => 'required', // Corregí 'usuarios' a 'usuario'
+            'quejas_sugerencias' => 'nullable',
+        ]);
 
-    Registro::create([
-        'nombre' => $request->nombre,
-        'cuenta' => $request->cuenta,
-        'servicio' => $request->servicio,
-        'numero_equipo' => $request->numero_equipo, 
-        'licenciaturas' => $request->licenciaturas,
-        'usuarios' => $request->usuarios,
-        'quejas_sugerencias' => $request->quejas_sugerencias,
-    ]);
+        try {
+            Registro::create([
+                'nombre' => $request->nombre,
+                'cuenta' => $request->cuenta,
+                'servicio' => $request->servicio,
+                'numero_equipo' => $request->numero_equipo, 
+                'licenciaturas' => $request->licenciaturas,
+                'usuarios' => $request->usuario, // Corregí 'usuarios' a 'usuario'
+                'quejas_sugerencias' => $request->quejas_sugerencias,
+            ]);
 
-    return redirect()->route('registro')->with('success', 'Los datos se han guardado correctamente.');}
+            return redirect()->route('registro')->with('success', 'Los datos se han guardado correctamente.');
+        } catch (\Exception $e) {
+            $errorMessage = 'Ha ocurrido un error al guardar los datos. Detalles: ' . $e->getMessage();
+            return redirect()->route('registro')->with('error', $errorMessage);
+        }
+        
+    }
 }
--- RegistrosFacico/database/migrations/2023_07_26_192137_create_registros_table.php
+++ RegistrosFacico/database/migrations/2023_07_26_192137_create_registros_table.php
@@ -7,19 +7,19 @@ use Illuminate\Support\Facades\Schema;
 class CreateRegistrosTable extends Migration
 {
     public function up()
-    {
-        Schema::create('registros', function (Blueprint $table) {
-            $table->id();
-            $table->string('nombre');
-            $table->string('cuenta');
-            $table->string('servicio');
-            $table->string('numero_equipo'); 
-            $table->string('licenciaturas');
-            $table->string('usuarios');
-            $table->string('quejas_sugerencias');
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('registros', function (Blueprint $table) {
+        $table->id();
+        $table->string('nombre');
+        $table->string('cuenta');
+        $table->string('servicio');
+        $table->string('numero_equipo')->nullable(); // Establecer como nullable
+        $table->string('licenciaturas');
+        $table->string('usuarios');
+        $table->string('quejas_sugerencias')->nullable(); // Establecer como nullable
+        $table->timestamps();
+    });
+}
     
     public function down()
     {
