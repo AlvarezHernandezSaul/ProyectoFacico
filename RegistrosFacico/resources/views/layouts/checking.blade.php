bfd42d7a5d21c0a2d0bf133784940eb55248356d
AlvarezHernandezSaul <al222110813@gmail.com>
2023-07-29 13:38:33 -0600

    Ortografia

--- RegistrosFacico/resources/views/layouts/checking.blade.php
+++ RegistrosFacico/resources/views/layouts/checking.blade.php
@@ -11,7 +11,7 @@
     <div class="d-flex justify-content-between">
         <a class="text-start fs-4 text-dark">Bienvenid@  <b> {{ auth()->user()->name }} </b> </a>
         <div class="text-end">
-            <a class="btn btn-danger" href="{{ route('login.destroy') }}">Cerrar Sesión</a>
+            <a class="btn btn-danger" href="{{ route('login.destroy') }}">Cerrar sesión</a>
         </div>
     </div>
     
@@ -19,7 +19,7 @@
 @else
 <div class="d-flex justify-content-between align-items-center">
     <p><a class="btn text-white mt-2 mt-md-0" href="{{route('registroreg')}}" style="background-color: #2d5135;">Inicio</a></p>
-    <h1 class="container fs-4 fw-bold text-center">Iniciar Sesión</h1>
+    <h1 class="container fs-4 fw-bold text-center">Iniciar sesión</h1>
     <p><a class="btn text-white mt-2 mt-md-0" href="{{route('register.index')}}" style="background-color: #2d5135;">Registrarse</a></p>
 </div>
 @endif
